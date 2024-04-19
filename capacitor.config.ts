ce83554b26564b0a5373e478febfede8f5c760dd
himisaran17 <himanshu@legalxgen.com>
2024-04-19 18:10:42 +0530

    himanshu new app

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'lxg-mobile-new',
+  webDir: 'dist'
+};
+
+export default config;
